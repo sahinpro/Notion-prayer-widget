42d44aaf36a32a8fc137eabb16301f4804042147
sahinhub <sahinpro@users.noreply.github.com>
2026-07-09 19:55:00 +0600

    bg transparent

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import PrayerTimesWidget from "./PrayerTimesWidget";
 export default function App() {
   return (
     <main className="relative min-h-screen w-full overflow-hidden bg-[#191919]">
-      <div className="relative z-10 w-full px-3 py-3 sm:px-4">
+      <div className="relative z-10 w-full ">
         <PrayerTimesWidget />
       </div>
     </main>
